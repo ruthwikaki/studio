5e3777159e69c8f010e0f300accb50219d24d5e6
aki ruthwik39 <aki.ruthwik39@gmail.com>
2025-06-20 23:52:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`} suppressHydrationWarning={true}>
         <QueryProvider>
           <AuthProvider>
             {children}
